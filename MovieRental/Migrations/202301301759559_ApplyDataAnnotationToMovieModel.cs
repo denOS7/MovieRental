614f1a783813b36eee92f0a5e868fc835bd11c47
Bit Kova <deniskreci@gmail.com>
2023-02-02 15:45:09 +0100

    Added validation check for ModelState and a simple validation message in CustomerForm view.

---
+++ MovieRental/Migrations/202301301759559_ApplyDataAnnotationToMovieModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieRental.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyDataAnnotationToMovieModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+        }
+    }
+}
--- MovieRental/Models/Movie.cs
+++ MovieRental/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +10,22 @@ namespace MovieRental.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Required]
         [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Required]
         [Display(Name = "Genre")]
         public byte GenreId { get; set; }
     }
